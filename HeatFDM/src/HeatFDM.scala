e9d33d2dbe252f6fb517092815172284931272f8
Filip Tubic <tubefilip@gmail.com>
2016-11-08 12:48:44 +0100

    Heat finish

--- HeatFDM/src/HeatFDM.scala
+++ HeatFDM/src/HeatFDM.scala
@@ -9,17 +9,17 @@ import org.apache.spark.rdd.RDD
 import org.apache.log4j.{Level, Logger}
 
 object HeatFDM {
-  val size: Int = 4
-  val T = 100
-  val k = 1
+  val size: Int = 100
+  val N = 200
+  val k = 1.0d / (size - 1.0d)
   //val h = 0.2
-  val h = 1
+  val h = 0.25
   val r = k / (h * h)
   var points: Array[(Int, Double)] = Array()
   var data: RDD[(Int, Double)] = null
 
   def interior(ix: (Int, Double)): Boolean = {
-    if (ix._1 > 0 && (ix._1 < size)) return true else return false
+    if (ix._1 > 0 && (ix._1 < (size -1))) return true else return false
   }
 
   def stencil(x: (Int, Double)) = {
@@ -43,16 +43,18 @@ object HeatFDM {
 
     //FDM Heat parallel
     points :+= (0, 0.0d)
-    for (i <- 1 until size by k) {
-      points :+= (i, 100.0d * Math.sin(Math.PI * i))
+    for (i <- 1 until (size - 1) by 1) {
+      points :+= (i, 100.0d * Math.sin(Math.PI * i * k))
     }
+    
     points :+= (size, 0.0d)
     data = sc.parallelize(points)
 
-    for (i <- 1 to T by k) {
+    for (i <- 1 to N by 1) {
       val stencilParts = data.flatMap(x => stencil(x))
       data = stencilParts.reduceByKey((x, y) => x + y)
     }
+
     data.sortBy(x => x._1, true).foreach(println)
   }
 }
